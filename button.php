3de74727bb54148f4c1b3536928039f9613bccb5
Anastasiia Arseneva <uminormail@gmail.com>
2020-04-03 02:45:09 +0300

    Button

---
+++ button.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ru-RU" itemscope itemtype="http://schema.org/WebSite">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="button.css">
+    <!--    ЗДЕСЬ (НИЖЕ) ПОДКЛЮЧАЕТСЯ ШРИФТ!! -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!--    ЗДЕСЬ (НИЖЕ) ПОДКЛЮЧАЕТСЯ AWESOME! -->
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <title></title>
+</head>
+
+<body>
+<section id="section-addform" class="section-addform-wrapper leaflet-control" data-leaflet-control-position="bottomleft">
+    <div style="text-align: right">
+        <button id="actor-addform-toggle" data-actor-url="/add" class="section-addform-button-toggle" data-content="section-addform-content" data-content-is-visible="false">ДОБАВИТЬ</button>
+    </div>
+</section>
+</body>
